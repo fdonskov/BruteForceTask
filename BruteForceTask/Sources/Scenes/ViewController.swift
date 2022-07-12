69fcb2e798328db5cdb1d8f768fe93aeed00990b
Fedor Donskov <fedya_donskov@mail.ru>
2022-07-12 07:40:10 +0300

    Create string extension

--- BruteForceTask/Sources/Scenes/ViewController.swift
+++ BruteForceTask/Sources/Scenes/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var button: UIButton!
+    
+    private var button: UIButton!
     
     var isBlack: Bool = false {
         didSet {
@@ -47,25 +48,6 @@ class ViewController: UIViewController {
         }
         
         print(password)
-    }
-}
-
-
-
-extension String {
-    var digits:      String { return "0123456789" }
-    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
-    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
-    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
-    var letters:     String { return lowercase + uppercase }
-    var printable:   String { return digits + letters + punctuation }
-
-
-
-    mutating func replace(at index: Int, with character: Character) {
-        var stringArray = Array(self)
-        stringArray[index] = character
-        self = String(stringArray)
     }
 }
 
